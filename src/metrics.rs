b997a4a27dbc4a5eff41ecbb35c4b6f75adebcf3
Ted Pilcher <tedspilcher@gmail.com>
2025-03-08 12:49:36 +0000

    Add fn to register metrics

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,7 +1,8 @@
 use lazy_static::lazy_static;
-use prometheus::{register_histogram, register_int_counter, Histogram, IntCounter};
+use prometheus::{register_histogram, register_int_counter, Histogram, IntCounter, Registry};
 
 lazy_static! {
+    pub static ref REGISTRY: Registry = Registry::new();
     pub static ref ORDER_COUNTER: IntCounter =
         register_int_counter!("order_counter", "Number orders recieved").unwrap();
     pub static ref TRADE_COUNTER: IntCounter =
@@ -11,4 +12,18 @@ lazy_static! {
         "Duration to match order with resting order"
     )
     .unwrap();
+}
+
+pub fn register_custom_metrics() {
+    REGISTRY
+        .register(Box::new(ORDER_COUNTER.clone()))
+        .expect("collector can be registered");
+
+    REGISTRY
+        .register(Box::new(TRADE_COUNTER.clone()))
+        .expect("collector can be registered");
+
+    REGISTRY
+        .register(Box::new(MATCHING_DURATION.clone()))
+        .expect("collector can be registered");
 }
