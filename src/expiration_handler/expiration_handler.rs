f9b3c31e37a1a9a8f22b0569e614f645d4ddbb6e
Ted Pilcher <ted.pilcher@moodys.com>
2025-03-11 20:37:08 +0000

    Implement method to insert expired order

--- src/expiration_handler/expiration_handler.rs
+++ src/expiration_handler/expiration_handler.rs
@@ -25,6 +25,23 @@ impl ExpirationHandler {
             expiration_queue: PriorityQueue::new(),
         }
     }
+
+    fn insert_expiring_order(
+        &mut self,
+        order_expiration_request: OrderExpirationRequest,
+    ) -> Result<()> {
+        if order_expiration_request.timestamp < Utc::now().timestamp() {
+            bail!("Timestamp in past")
+        }
+
+        self.expiration_queue.push(
+            order_expiration_request.order_uuid,
+            order_expiration_request.timestamp,
+        );
+
+        Ok(())
+    }
+
     fn send_cancellation_request(&mut self, order_id: Uuid) -> Result<()> {
         let order_request = OrderRequest::Cancel(CancelRequestType::Internal, order_id);
 
