0569a94713bf4bd9fc2e1c7a4343a64dca81123b
Ted Pilcher <ted.pilcher@moodys.com>
2025-03-12 16:14:01 +0000

    Update websever types to handle order expiration

--- src/web_server/mod.rs
+++ src/web_server/mod.rs
@@ -1,8 +1,12 @@
 use anyhow::anyhow;
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::orderbook::{Order, OrderSide, OrderType};
+use crate::{
+    expiration_handler::ExpirationOrderRequest,
+    orderbook::{Order, OrderSide, OrderType},
+};
 
 pub mod endpoints;
 
@@ -24,11 +28,13 @@ pub enum CancelRequestType {
 
 #[derive(Deserialize, Serialize)]
 pub struct TradeRequest {
+    pub id: Uuid,
     pub order_type: OrderType,
     pub order_side: OrderSide,
     pub price: Price,
     pub quantity: Quantity,
     pub minimum_quantity: Quantity,
+    pub expiration_date: Option<NaiveDateTime>,
 }
 
 impl TryFrom<TradeRequest> for Order {
@@ -40,8 +46,8 @@ impl TryFrom<TradeRequest> for Order {
         }
 
         Ok(Order {
+            id: trade_request.id,
             type_: trade_request.order_type,
-            id: Uuid::new_v4(),
             side: trade_request.order_side,
             price: trade_request.price,
             initial_quantity: trade_request.quantity,
@@ -52,5 +58,6 @@ impl TryFrom<TradeRequest> for Order {
 }
 
 pub struct AppState {
-    pub sender: crossbeam::channel::Sender<OrderRequest>,
+    pub order_engine_sender: crossbeam::channel::Sender<OrderRequest>,
+    pub order_expiration_sender: crossbeam::channel::Sender<ExpirationOrderRequest>,
 }
