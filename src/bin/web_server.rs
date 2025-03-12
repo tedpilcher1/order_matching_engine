5ef68ac3a0a27bd574b61aee9e60aaeeb47227be
Ted Pilcher <ted.pilcher@moodys.com>
2025-03-12 16:15:38 +0000

    Add new endpoint to webserver and create expiration handler

--- src/bin/web_server.rs
+++ src/bin/web_server.rs
@@ -3,11 +3,13 @@ use std::thread;
 use actix_web::{web, App, HttpServer};
 use crossbeam::channel::{self, Receiver};
 use order_matching_engine::{
+    expiration_handler::expiration_handler::ExpirationHandler,
     metrics::register_custom_metrics,
     orderbook::{orderbook::Orderbook, Order},
     web_server::{
         endpoints::{
-            cancel_order_endpoint, create_order_endpoint, metrics_endpoint, modify_order_endpoint,
+            cancel_order_endpoint, cancel_order_expiration_endpoint, create_order_endpoint,
+            metrics_endpoint, modify_order_endpoint,
         },
         AppState, OrderRequest,
     },
@@ -41,13 +43,24 @@ fn worker_thread(receiver: Receiver<OrderRequest>) {
 async fn main() -> std::io::Result<()> {
     register_custom_metrics();
 
-    let (sender, receiver) = channel::unbounded();
+    let (order_engine_sender, order_engine_receiver) = channel::unbounded();
+    let (order_expiration_sender, order_expiration_receiver) = channel::unbounded();
+    let cancellation_request_sender = order_engine_sender.clone();
 
     thread::spawn(move || {
-        worker_thread(receiver);
+        let mut expiration_handler =
+            ExpirationHandler::new(cancellation_request_sender, order_expiration_receiver);
+        expiration_handler.run();
     });
 
-    let state = web::Data::new(AppState { sender });
+    thread::spawn(move || {
+        worker_thread(order_engine_receiver);
+    });
+
+    let state = web::Data::new(AppState {
+        order_engine_sender,
+        order_expiration_sender,
+    });
 
     HttpServer::new(move || {
         App::new()
@@ -56,6 +69,7 @@ async fn main() -> std::io::Result<()> {
             .service(create_order_endpoint)
             .service(cancel_order_endpoint)
             .service(modify_order_endpoint)
+            .service(cancel_order_expiration_endpoint)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
